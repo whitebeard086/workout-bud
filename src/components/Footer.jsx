f6c54feafef883b0513bed1acfc334a179fd4a60
The Exorcist <whitebeard086@gmail.com>
2022-07-28 08:06:48 +0100

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,23 @@
+import { Box, Stack, Typography } from "@mui/material";
+
+import Logo from "../assets/images/Logo-1.png";
+
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
-export default Footer
+    <Box mt="80px" bgcolor="#FFF3F4">
+      <Stack gap="40px" alignItems="center" px="40px" pt="24px">
+        <img src={Logo} alt="logo" width="200px" height="40px" />
+      </Stack>
+      <Typography
+        variant="h5"
+        sx={{ fontSize: { lg: "28px", xs: "20px" } }}
+        mt="41px"
+        textAlign="center"
+        pb="40px"
+      >
+        Made with ❤️ by Alex
+      </Typography>
+    </Box>
+  );
+};
+export default Footer;
