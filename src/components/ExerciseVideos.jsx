b0afcb9baddc68f1b1a1920a8d0c362c1af882fd
The Exorcist <whitebeard086@gmail.com>
2022-07-28 08:20:57 +0100

    Deploy

--- src/components/ExerciseVideos.jsx
+++ src/components/ExerciseVideos.jsx
@@ -5,7 +5,7 @@ const ExerciseVideos = ({ exerciseVideos, name }) => {
 
   return (
     <Box sx={{ marginTop: { lg: "200px", xs: "20px" } }} p="20px">
-      <Typography variant="h3" mb="33px">
+      <Typography variant="h3" mb="33px" fontWeight="bold">
         Watch{" "}
         <span style={{ color: "#FF2625", textTransform: "capitalize" }}>
           {name}
--- src/components/SimilarExercises.jsx
+++ src/components/SimilarExercises.jsx
@@ -5,8 +5,8 @@ import { HorizontalScrollbar, Loader } from ".";
 const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => {
   return (
     <Box sx={{ mt: { lg: "100px", xs: "0" } }}>
-      <Typography variant="h3" mb={5}>
-        Exercises that target the same muscle group
+      <Typography variant="h3" mb={10} mt={15} textTransform="capitalize" fontWeight="bold">
+        similar <span style={{ color: '#FF2625' }}>target muscle</span> exercises
       </Typography>
       <Stack direction="row" sx={{ p: "2", position: "relative" }}>
         {targetMuscleExercises.length ? (
@@ -15,8 +15,8 @@ const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => {
           <Loader />
         )}
       </Stack>
-      <Typography variant="h3" mb={5}>
-        Exercises that use the same equipment
+      <Typography variant="h3" mb={10} mt={20} textTransform="capitalize" fontWeight="bold">
+        similar <span style={{ color: '#FF2625' }}>equipment</span> exercises
       </Typography>
       <Stack direction="row" sx={{ p: "2", position: "relative" }}>
         {equipmentExercises.length ? (
